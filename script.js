b2caa819981de5d7fff5b263baf4ebc631ec2fe9
bruno3du <bruno.3duardo@hotmail.com>
2021-09-20 21:32:25 -0300

    Finalizando estilização mobile

--- script.js
+++ script.js
@@ -3,14 +3,46 @@ document.addEventListener('DOMContentLoaded', () => {
 	gettingDatas();
 });
 
+const timeWork = document.getElementById('time-work');
+const timePlay = document.getElementById('time-play');
+const timeStudy = document.getElementById('time-study');
+const timeExercise = document.getElementById('time-exercise');
+const timeSocial = document.getElementById('time-social');
+const timeSelfCare = document.getElementById('time-selfcare');
+
+// let daily = {}
+let weekly = {};
+let monthly = {};
+
 function gettingDatas() {
-	fetch('./data.json')
+	return fetch('./data.json')
 		.then((res) => {
 			return res.json();
 		})
 		.then((json) => {
-			console.log(json);
+			return json.forEach((data) => {
+				daily = async() => {
+					return { title: data.title, timeframes: data.timeframes.daily };
+				};
+				weekly = {
+					title: data.title,
+					timeframes: data.timeframes.weekly,
+				};
+				monthly = {
+					title: data.title,
+					timeframes: data.timeframes.monthly,
+				};
+			});
 		});
 }
+
+// async function daily(json) {
+// 	time = await json;
+// 	console.log(time);
+// }
+
+// daily()
+
+console.log(weekly, monthly, gettingDatas());
 
 // Create container of dates and
